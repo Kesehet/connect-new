9b739f2023aab1768b6c9ff2811fe77780e2d96f
Kesehet <hamood.siddiqui@gmail.com>
2023-06-19 07:41:41 +0530

    asdasd

--- update.php
+++ update.php
@@ -1,3 +1,11 @@
 <?php
-            echo exec('git fetch');
-            echo exec('git merge origin/master');
+$output = [];
+$return_var = '';
+exec('git fetch', $output, $return_var);
+
+if ($return_var === 0) {
+    echo "Git fetch successful\n";
+} else {
+    echo "Git fetch failed\n";
+    echo "Error: " . implode("\n", $output) . "\n";
+}
